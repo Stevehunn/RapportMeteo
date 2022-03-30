b646b16a99877fcd3fd93d8eccd7a1756fe0f85f
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2022-03-30 23:56:36 +0200

    Ajout menu capteur

--- app/src/main/java/com/example/weatherreport/MainActivity.java
+++ app/src/main/java/com/example/weatherreport/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.weatherreport;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+
+        // If you want Icon display in Overflow Menu.
+        // https://stackoverflow.com/questions/19750635/icon-in-menu-not-showing-in-android
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+        return true;
     }
 }
