0ac929fc7a7ec96219e0a12f25c34f67349ecc45
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2022-03-30 23:37:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/weatherreport/Temperature.java
@@ -0,0 +1,25 @@
+package com.example.weatherreport;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Temperature extends AppCompatActivity {
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.afficheField);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://thingspeak.com/channels/1686204/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15");
+    }
+}
