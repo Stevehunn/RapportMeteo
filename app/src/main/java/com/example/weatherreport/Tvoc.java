d8e04133f9eb9eec173ac4279577a9a75b93a7d1
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2022-03-31 17:46:24 +0200

    Ajout capteur pollution Tvoc complet

--- app/src/main/java/com/example/weatherreport/Tvoc.java
+++ app/src/main/java/com/example/weatherreport/Tvoc.java
@@ -12,55 +12,46 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Pollution extends Activity {
+public class Tvoc extends Activity {
     private WebView webView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pollution);
+        setContentView(R.layout.tvoc);
+
 
         Button buttonHome = (Button) findViewById(R.id.buttonHome);
+        Button buttonVersHum = (Button) findViewById(R.id.buttonVersHum);
         Button buttonRefresh = (Button) findViewById(R.id.buttonRefresh);
-        Button buttonVersTvoc = (Button) findViewById(R.id.buttonVersTvoc);
 
-        webView = (WebView) findViewById(R.id.afficheFieldPol);
+        webView = (WebView) findViewById(R.id.afficheFieldTvoc);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setInitialScale(240);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://thingspeak.com/channels/1686204/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Mesure+Pollution&type=line");
+        webView.loadUrl("https://thingspeak.com/channels/1686204/charts/5?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Mesure+TVOC&type=line");
+
 
         //Bouton pour aller vers la page d'accueil
 
         buttonHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i("Temperature", "Home");
-                Intent i = new Intent(Pollution.this, MainActivity.class);
+                Intent i = new Intent(Tvoc.this, MainActivity.class);
                 startActivity(i);
                 onPause();
             }
         });
 
-        //Bouton pour aller vers le capteur TVOC
-        buttonVersTvoc.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.i("Temperature", "versHum");
-
-                Intent i = new Intent(Pollution.this, Tvoc.class);
-                startActivity(i);
-                onPause();
-            }
-        });
 
         //Bouton pour refresh
         buttonRefresh.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i("refresh", "refresh");
-                Pollution.this.webView.loadUrl("https://thingspeak.com/channels/1686204/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Mesure+Pollution&type=line");
+                Tvoc.this.webView.loadUrl("https://thingspeak.com/channels/1686204/charts/5?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Mesure+TVOC&type=line");
             }
         });
-
     }
 }
