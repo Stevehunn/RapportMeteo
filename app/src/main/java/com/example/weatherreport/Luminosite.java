0ccf23f8bca6c79161d7ba5c51988b03005129ba
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2022-04-01 14:03:45 +0200

    Correction Luminosite

--- app/src/main/java/com/example/weatherreport/Luminosite.java
+++ app/src/main/java/com/example/weatherreport/Luminosite.java
@@ -13,47 +13,48 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Luminosite extends Activity {
-
     private WebView webView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.luminosite);
 
+
         Button buttonHome = (Button) findViewById(R.id.buttonHome);
         Button buttonVersPol = (Button) findViewById(R.id.buttonVersPol);
         Button buttonRefresh = (Button) findViewById(R.id.buttonRefresh);
 
-        webView = (WebView) findViewById(R.id.afficheFieldLum);
+        webView = (WebView) findViewById(R.id.afficheFieldPol);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setInitialScale(220);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://thingspeak.com/channels/1686204/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Mesure+Luminosit%C3%A9&type=line");
 
+
         //Bouton pour aller vers la page d'accueil
 
         buttonHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.i("Lum", "Home");
+                Log.i("Luminosite", "Home");
                 Intent i = new Intent(Luminosite.this, MainActivity.class);
                 startActivity(i);
                 onPause();
             }
         });
 
-        //Bouton pour aller vers le capteur de Pollution
+        //Bouton pour aller vers le capteur d'humidité
         buttonVersPol.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.i("Lum", "versPol");
+                Log.i("Luminosite", "versPol");
 
                 Intent i = new Intent(Luminosite.this, Pollution.class);
                 startActivity(i);
                 onPause();
             }
         });
-
 
         //Bouton pour refresh
         buttonRefresh.setOnClickListener(new View.OnClickListener() {
